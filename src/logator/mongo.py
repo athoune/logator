dc8a05832eef61ecc881c26b6b3c5ffea959efda
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-21 21:57:28 +0200

    some search

--- src/logator/mongo.py
+++ src/logator/mongo.py
@@ -7,7 +7,7 @@ import weblog
 import log
 
 from pymongo import Connection
-
+from bson.code import Code
 
 class MongoStore(object):
 	def __init__(self):
@@ -28,10 +28,38 @@ if __name__ == '__main__':
 			cpt +=1
 			if cpt % 5000 == 0:
 				print cpt
-			store.insert(line.as_dict())
+			try:
+				store.insert(line.as_dict())
+			except Exception as inst:
+				print inst
+				print line.as_dict()
 	else:
-		print "404 not found :", store.db.logs.find({'code':404}).count()
-		print "User agent :", store.db.logs.distinct('userAgent.family')
-		print "Country :", store.db.logs.distinct('ip2something.country_name')
+		print store.db.logs.count(), 'documents'
+		#print "404 not found :", store.db.logs.find({'code':404}).count()
+		#print "User agent :", store.db.logs.distinct('userAgent.family')
+		#print "Country :", store.db.logs.distinct('ip2something.country_name')
+		map_ = Code("""
+			function() {
+				emit(this.ip2something.country_name, {size: this.size});
+			}
+		""")
+		reduce_ = Code("""
+			function(key, values) {
+				var sum = 0;
+				var n = 0;
+				values.forEach(function(doc) {
+					sum += doc.size;
+					n += 1;
+				});
+				return {size: sum, count: n};
+			}
+		""")
+		result = store.db.logs.map_reduce(map_, reduce_) #limit=1000 full_response=True
+		print result
+		for doc in result.find():
+			print doc
+		#for doc in store.db.logs.find(limit=20):
+			#print doc
+			#print doc['url'], doc['size']
 		#for log in store.db.logs.find({'code':404}):
 		#	print log
