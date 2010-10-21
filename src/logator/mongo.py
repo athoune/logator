3ff58e24fdcb75c8e8751968f4c5c36714fb39cc
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-21 23:20:20 +0200

    bulk insert

--- src/logator/mongo.py
+++ src/logator/mongo.py
@@ -3,6 +3,8 @@
 
 __author__ = "Mathieu Lecarme <mathieu@garambrogne.net>"
 
+import time
+
 import weblog
 import log
 
@@ -14,7 +16,25 @@ class MongoStore(object):
 		connection = Connection()
 		self.db = connection.test_log
 	def insert(self, line):
-		self.db.logs.insert(line)
+		try:
+			self.db.logs.insert(line)
+		except Exception as inst:
+			print inst
+			print line
+	def bulk_insert(self, lines):
+		cpt = 0
+		buff = []
+		for line in lines:
+			cpt += 1
+			if cpt % 5000 == 0:
+				print cpt
+			if line != None:
+				buff.append(line.as_dict())
+			if cpt % 500 == 0:
+				self.insert(buff)
+				buff = []
+		if len(buff) > 0:
+			self.insert(buff)
 
 if __name__ == '__main__':
 	import sys
@@ -23,18 +43,30 @@ if __name__ == '__main__':
 
 	if len(sys.argv) == 1:
 		class Line(weblog.Lighttpd, weblog.IP2Something, weblog.UserAgent): pass
-		cpt = 0
-		for line in log.log(Line, sys.stdin):
-			cpt +=1
-			if cpt % 5000 == 0:
-				print cpt
-			try:
-				store.insert(line.as_dict())
-			except Exception as inst:
-				print inst
-				print line.as_dict()
+		store.bulk_insert(log.log(Line, sys.stdin))
 	else:
+		store.db.logs.ensure_index('code')
 		print store.db.logs.count(), 'documents'
+		print "codes :"
+		chrono = time.time()
+		for code in store.db.logs.distinct('code'):
+			print "\t", code, ' :', store.db.logs.find({'code':code}).count()
+		print time.time() - chrono, 's'
+		#chrono = time.time()
+		map_ = Code('function() { emit(this.code, {cpt:1}); }')
+		reduce_ = Code('''
+			function(key, values) {
+				var sum = 0;
+				values.forEach(function(doc){
+					sum += 1;
+				});
+				return {cpt:sum};
+			}
+		''')
+		#result = store.db.logs.map_reduce(map_, reduce_) #limit=1000 full_response=True
+		#for doc in result.find():
+		#	print doc
+		#print time.time() - chrono, 's'
 		#print "404 not found :", store.db.logs.find({'code':404}).count()
 		#print "User agent :", store.db.logs.distinct('userAgent.family')
 		#print "Country :", store.db.logs.distinct('ip2something.country_name')
@@ -54,10 +86,9 @@ if __name__ == '__main__':
 				return {size: sum, count: n};
 			}
 		""")
-		result = store.db.logs.map_reduce(map_, reduce_) #limit=1000 full_response=True
-		print result
-		for doc in result.find():
-			print doc
+		#result = store.db.logs.map_reduce(map_, reduce_) #limit=1000 full_response=True
+		#for doc in result.find():
+		#	print doc
 		#for doc in store.db.logs.find(limit=20):
 			#print doc
 			#print doc['url'], doc['size']
