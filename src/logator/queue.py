2eb89e5c9fb7897bb9f7941a6e6ac7cb42af1c03
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-23 19:33:42 +0200

    less import, more simple loop

--- src/logator/queue.py
+++ src/logator/queue.py
@@ -1,28 +1,21 @@
 import zmq
 import sys
 
-import mongo
-import weblog
-import log
-
 c = zmq.Context()
 s = c.socket(zmq.REP)
-s.bind('tcp://127.0.0.1:10001')
-
-class Line(weblog.Lighttpd, weblog.IP2Something, weblog.UserAgent): pass
-
-l = iter(sys.stdin)
+#s.bind('tcp://127.0.0.1:10001')
+s.bind('ipc:///tmp/logator')
 
 print "waiting for workers"
 cpt = 0
-while True:
+for line in sys.stdin:
+	msg = s.recv(copy=False)
+	s.send(line)
 	cpt += 1
 	if cpt % 5000 == 0:
 		print cpt
+	
+while True:
 	msg = s.recv(copy=False)
-	try:
-		s.send(l.next())
-	except StopIteration:
-		s.send('')
-		break
+	s.send('')
 
