4a87bdc8d26d319a1120a2cc059bcc2dd352de5e
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-23 21:44:36 +0200

    speed control

--- src/logator/queue.py
+++ src/logator/queue.py
@@ -1,5 +1,6 @@
 import zmq
 import sys
+import time
 
 c = zmq.Context()
 s = c.socket(zmq.REP)
@@ -8,12 +9,14 @@ s.bind('ipc:///tmp/logator')
 
 print "waiting for workers"
 cpt = 0
+chrono = time.time()
 for line in sys.stdin:
 	msg = s.recv(copy=False)
 	s.send(line)
 	cpt += 1
 	if cpt % 5000 == 0:
-		print cpt
+		print cpt, 5000 / (time.time() - chrono), "lines/second"
+		chrono = time.time()
 	
 while True:
 	msg = s.recv(copy=False)
