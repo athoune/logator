4a058a6aa11555255c930e321e8ba778ac2d7e51
Mathieu Lecarme <mathieu@garambrogne.net>
2010-10-23 19:34:26 +0200

    more explicit vars

--- src/logator/worker.py
+++ src/logator/worker.py
@@ -6,18 +6,19 @@ from mongo import MongoStore
 
 c = zmq.Context()
 s = c.socket(zmq.REQ)
-s.connect('tcp://127.0.0.1:10001')
+#s.connect('tcp://127.0.0.1:10001')
+s.connect('ipc:///tmp/logator')
 
-class Machin(object):
+class Logs(object):
 	def __iter__(self):
 		while True:
 			s.send('', copy=False)
-			msg2 = s.recv(copy=False)
-			if msg2 == '': break
-			yield str(msg2)
+			line = s.recv(copy=False)
+			if line == '': break
+			yield str(line)
 
 class Line(weblog.Lighttpd, weblog.IP2Something, weblog.UserAgent): pass
 
 store = MongoStore()
 
-store.bulk_insert(log.log(Line, Machin()))
+store.bulk_insert(log.log(Line, Logs()))
