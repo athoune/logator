76520b5106c3a59ca4a771f2cc6b9466e9cb744e
Mathieu Lecarme <mathieu@garambrogne.net>
2010-12-27 10:55:24 +0100

    more doc

--- src/logator/syslogd.py
+++ src/logator/syslogd.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """
 http://www.faqs.org/rfcs/rfc3164.html
 """
+
+__author__ = "Mathieu Lecarme <mathieu@garambrogne.net>"
 
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
@@ -44,15 +49,22 @@ facilities = [
 ]
 
 class SyslogInput(DatagramProtocol):
-	def processLine(self, level, facility, month, day, hour, host, txt):
-		print "\x1b[31m%s\x1b[39;49;00m\x1b[32m@%s\x1b[39;49;00m [\x1b[33m%s\x1b[39;49;00m] %s" % (levels[level], host, facilities[facility], txt)
+	"""
+	Syslog UDP listener
+	"""
+	def processLine(self, level, facility, month, day, hour, host, msg):
+		"""
+		One line of log
+		override me !
+		"""
+		print "\x1b[31m%s\x1b[39;49;00m\x1b[32m@%s\x1b[39;49;00m [\x1b[33m%s\x1b[39;49;00m] %s" % (levels[level], host, facilities[facility], msg)
 	def datagramReceived(self, data, (host, port)):
 		n = data.find(">")
 		a = int(data[1:n])
 		level = a & 0b00000111
 		facility = (a & 0b01111000) / 8
-		(month, day, hour, host, txt) = data[n+1:].split(' ', 4)
-		self.processLine(level, facility, month, day, hour, host, txt)
+		(month, day, hour, host, msg) = data[n+1:].split(' ', 4)
+		self.processLine(level, facility, month, day, hour, host, msg)
 
 if __name__ == '__main__':
 	reactor.listenUDP(514, SyslogInput())
